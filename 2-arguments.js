a04eaa7ce3c58baf13fc0ddfbbdad0796c7caec7
jesse-aj <appiahjesse123@gmail.com>
2025-07-13 15:31:51 +0000

    fix the error on how to pass in arguments in java

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,11 +1,11 @@
 const args = process.argv.slice(2);
-const count = args.lenght;
+const count = args.length;
 
-if (count === 0){
-    console.log ("No argument");
-
-}else if (count == 1){
-    console.log("Argument found");
-}else {
-    console.log("Arguments found");
+if (count === 0) {
+  console.log("No argument");
+} else if (count === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
 }
+

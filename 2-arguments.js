e66e1088bf31a26c3dcec1f87241c6f216399e8f
jesse-aj <appiahjesse123@gmail.com>
2025-07-13 15:29:04 +0000

    fix how to pass in arguments in java

--- 2-arguments.js
+++ 2-arguments.js
@@ -5,7 +5,7 @@ if (count === 0){
     console.log ("No argument");
 
 }else if (count == 1){
-    console.log("Argument found")
+    console.log("Argument found");
 }else {
     console.log("Arguments found");
 }
